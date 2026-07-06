8449110fa34811d513958cb775ca30e3f636513a
Ubuntu <ubuntu@ip-172-31-34-16.ec2.internal>
2026-07-06 10:33:16 +0000

    Initial Monitering script

---
+++ monitor.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "================================="
+echo "EC2 Monitoring Report"
+echo "Date: $(Date)"
+echo "====================================="
+
+
+echo ""
+echo "Running Services"
+systemctl list-units --type=service --state=running
+echo""
+echo "CPU Usage:"
+top -bn1 | grep "Cpu(S)"
+
+
+echo ""
+echo "Memory Usage:"
+free -h
+
+echo ""
+echo "Disk Usage:"
+df -h
+
+echo ""
+echo "================================"
